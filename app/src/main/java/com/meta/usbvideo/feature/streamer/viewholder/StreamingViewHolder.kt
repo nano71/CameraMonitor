99a2e5e34d884eaca4479b76a27aa5cbcb7a1d23
nano71 <80870408+nano71@users.noreply.github.com>
2026-02-26 16:01:26 +0800

    Frame rate refresh and ToggleTip no longer depend on onSurfaceTextureUpdated

--- app/src/main/java/com/meta/usbvideo/feature/streamer/viewholder/StreamingViewHolder.kt
+++ app/src/main/java/com/meta/usbvideo/feature/streamer/viewholder/StreamingViewHolder.kt
@@ -34,121 +34,70 @@ class StreamingViewHolder(
     private val streamerViewModel: StreamerViewModel,
 ) : RecyclerView.ViewHolder(rootView) {
 
-    private var overlayMode: OverlayMode = OverlayMode.INITIAL_STATS
     val streamingStats: TextView = rootView.findViewById(R.id.streaming_stats)
     val videoFrame: VideoContainerView = rootView.findViewById(R.id.video_container)
     private var lastUpdatedAt = 0L
-    private var stateTransitionAt = 0L
 
     init {
         val videoTextureView = TextureView(videoFrame.context)
-        videoTextureView.surfaceTextureListener =
-            object : TextureView.SurfaceTextureListener {
-                override fun onSurfaceTextureAvailable(
-                    surfaceTexture: SurfaceTexture,
-                    width: Int,
-                    height: Int
-                ) {
-                    Log.i(
-                        TAG,
-                        "onSurfaceTextureAvailable() called with: surface = $surfaceTexture, width = $width, height = $height"
-                    )
-                    streamerViewModel.surfaceTextureAvailable(surfaceTexture, width, height)
-                }
-
-                override fun onSurfaceTextureSizeChanged(
-                    surface: SurfaceTexture,
-                    width: Int,
-                    height: Int
-                ) {
-                    videoFrame.invalidate()
-                    Log.i(
-                        TAG,
-                        "onSurfaceTextureSizeChanged() called with: surface = $surface, width = $width, height = $height"
-                    )
-                }
-
-                override fun onSurfaceTextureDestroyed(surfaceTexture: SurfaceTexture): Boolean {
-                    Log.i(TAG, "onSurfaceTextureDestroyed() called with: surface = $surfaceTexture")
-                    videoFrame.updateVideoInfo("")
-                    streamerViewModel.surfaceTextureDestroyed()
-                    return true
-                }
-
-                override fun onSurfaceTextureUpdated(surfaceTexture: SurfaceTexture) {
-                    val now = SystemClock.uptimeMillis()
-                    if (now - lastUpdatedAt > 999) {
-                        videoFrame.updateVideoInfo(streamerViewModel.getVideoStreamInfoString())
-
-                        if (overlayMode != OverlayMode.NONE) {
-                            val streamingStatsSummaryText =
-                                if (overlayMode == OverlayMode.TOGGLE_TIP) {
-                                    rootView.context.getText(R.string.streaming_stats_toggle_tip)
-                                } else {
-                                    ""
-                                }
-                            streamingStats.text = streamingStatsSummaryText
-                            streamingStats.isVisible = streamingStatsSummaryText.isNotEmpty()
-                        }
-                        lastUpdatedAt = now
-                    }
-                    if (overlayMode == OverlayMode.NONE) {
-                        return
-                    }
-
-                    if (stateTransitionAt == 0L) {
-                        stateTransitionAt = now
-                    } else {
-                        when (overlayMode) {
-                            OverlayMode.INITIAL_STATS -> {
-                                if (now - stateTransitionAt > 10_000) {
-                                    updateOverlayMode(overlayMode.next())
-                                }
-                            }
-
-                            OverlayMode.TOGGLE_TIP -> {
-                                if (now - stateTransitionAt > 5_000) {
-                                    updateOverlayMode(overlayMode.next())
-                                }
-                            }
-
-                            else -> Unit
-                        }
-                    }
-                }
-            }
-
+        videoTextureView.surfaceTextureListener = setupSurfaceTextureListener()
         val videoFormat = streamerViewModel.videoFormat
         val width = videoFormat?.width ?: 1920
         val height = videoFormat?.height ?: 1080
         videoFrame.addVideoTextureView(videoTextureView, width, height)
         videoFrame.updateVideoInfo(streamerViewModel.getVideoStreamInfoString())
-        if (overlayMode == OverlayMode.STREAMING_STATS) {
-            updateOverlayMode(OverlayMode.NONE)
-        } else {
-            updateOverlayMode(overlayMode.next())
-        }
+        showToggleTip()
     }
 
-
-    private enum class OverlayMode {
-        INITIAL_STATS,
-        TOGGLE_TIP,
-        NONE,
-        STREAMING_STATS,
-    }
-
-    inline fun <reified T : Enum<T>> T.next(): T {
-        val values = enumValues<T>()
-        val nextOrdinal = (ordinal + 1) % values.size
-        return values[nextOrdinal]
-    }
-
-    private fun updateOverlayMode(overlayMode: OverlayMode) {
-        this.overlayMode = overlayMode
-        if (overlayMode == OverlayMode.NONE) {
+    private fun showToggleTip() {
+        streamingStats.text = rootView.context.getText(R.string.streaming_stats_toggle_tip)
+        streamingStats.isVisible = true
+        rootView.postDelayed({
             streamingStats.isVisible = false
+        }, 5000L)
+    }
+
+
+    fun setupSurfaceTextureListener(): TextureView.SurfaceTextureListener {
+        return object : TextureView.SurfaceTextureListener {
+            override fun onSurfaceTextureAvailable(
+                surfaceTexture: SurfaceTexture,
+                width: Int,
+                height: Int
+            ) {
+                Log.i(
+                    TAG,
+                    "onSurfaceTextureAvailable() called with: surface = $surfaceTexture, width = $width, height = $height"
+                )
+                streamerViewModel.surfaceTextureAvailable(surfaceTexture, width, height)
+            }
+
+            override fun onSurfaceTextureSizeChanged(
+                surface: SurfaceTexture,
+                width: Int,
+                height: Int
+            ) {
+                videoFrame.invalidate()
+                Log.i(
+                    TAG,
+                    "onSurfaceTextureSizeChanged() called with: surface = $surface, width = $width, height = $height"
+                )
+            }
+
+            override fun onSurfaceTextureDestroyed(surfaceTexture: SurfaceTexture): Boolean {
+                Log.i(TAG, "onSurfaceTextureDestroyed() called")
+                videoFrame.updateVideoInfo("")
+                streamerViewModel.surfaceTextureDestroyed()
+                return true
+            }
+
+            override fun onSurfaceTextureUpdated(surfaceTexture: SurfaceTexture) {
+                val now = SystemClock.uptimeMillis()
+                if (now - lastUpdatedAt > 999) {
+                    videoFrame.updateVideoInfo(streamerViewModel.getVideoStreamInfoString())
+                    lastUpdatedAt = now
+                }
+            }
         }
-        stateTransitionAt = SystemClock.uptimeMillis()
     }
 }
